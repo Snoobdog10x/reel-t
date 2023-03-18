fe13b42d24001ae96ebc66f05158b6987a9ce211
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-03-18 22:49:21 +0700

    add navigation screen and bar

--- lib/screens/navigation/navigation_screen.dart
+++ lib/screens/navigation/navigation_screen.dart
@@ -5,9 +5,10 @@ import 'package:reel_t/screens/abstracts/abstract_state.dart';
 import 'package:reel_t/screens/feed/feed_screen.dart';
 import 'package:reel_t/screens/navigation/navigation_provider.dart';
 import 'package:reel_t/screens/notification/notification_screen.dart';
-import 'package:reel_t/screens/profile_screen.dart';
+import 'package:reel_t/screens/profile/profile_screen.dart';
 import 'package:reel_t/screens/search/search_screen.dart';
-import 'package:reel_t/shared_product/widgets/default_appbar.dart';
+
+import '../../shared_product/assets/icon/tik_tok_icons_icons.dart';
 
 class NavigationScreen extends StatefulWidget {
   const NavigationScreen({super.key});
@@ -20,6 +21,7 @@ enum NavigationPage { FEED, SEARCH, NOTIFICATION, PROFILE }
 
 class _NavigationScreenState extends AbstractState<NavigationScreen> {
   late NavigationProvider provider;
+  int currentScreen = NavigationPage.FEED.index;
   Map<int, Widget> pages = {
     NavigationPage.FEED.index: FeedScreen(),
     NavigationPage.SEARCH.index: SearchScreen(),
@@ -48,10 +50,12 @@ class _NavigationScreenState extends AbstractState<NavigationScreen> {
       builder: (context, child) {
         return Consumer<NavigationProvider>(
           builder: (context, value, child) {
-            var body = buildBody();
+            var body = pages[currentScreen];
             return buildScreen(
-              appBar: DefaultAppBar(appBarTitle: "sample appbar"),
               body: body,
+              background: Colors.black,
+              isSafe: false,
+              bottomNavBar: buildBottomBar(),
             );
           },
         );
@@ -59,9 +63,126 @@ class _NavigationScreenState extends AbstractState<NavigationScreen> {
     );
   }
 
+  Widget buildBottomBar() {
+    return Container(
+      width: screenWidth(),
+      height: screenHeight() * 0.1,
+      child: Row(
+        children: [
+          Expanded(
+            child: buildBottomBarItem(
+              "Feed",
+              TikTokIcons.home,
+              currentScreen == NavigationPage.FEED.index,
+              onTap: () {
+                currentScreen = NavigationPage.FEED.index;
+                notifyDataChanged();
+              },
+            ),
+          ),
+          Expanded(
+            child: buildBottomBarItem(
+              "Search",
+              TikTokIcons.search,
+              currentScreen == NavigationPage.SEARCH.index,
+              onTap: () {
+                currentScreen = NavigationPage.SEARCH.index;
+                notifyDataChanged();
+              },
+            ),
+          ),
+          Expanded(child: buildAddVideoButton()),
+          Expanded(
+            child: buildBottomBarItem(
+              "Notification",
+              TikTokIcons.messages,
+              currentScreen == NavigationPage.NOTIFICATION.index,
+              onTap: () {
+                currentScreen = NavigationPage.NOTIFICATION.index;
+                notifyDataChanged();
+              },
+            ),
+          ),
+          Expanded(
+            child: buildBottomBarItem(
+              "Profile",
+              TikTokIcons.profile,
+              currentScreen == NavigationPage.PROFILE.index,
+              onTap: () {
+                currentScreen = NavigationPage.PROFILE.index;
+                notifyDataChanged();
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildAddVideoButton({Function? onTap}) {
+    return GestureDetector(
+      onTap: () {
+        onTap?.call();
+      },
+      child: Column(
+        children: [
+          SizedBox(height: 8),
+          Container(
+            height: 40,
+            width: 60,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.white,
+            ),
+            child: Text(
+              "+",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildBottomBarItem(
+    String? title,
+    IconData? icon,
+    bool isSelect, {
+    Function? onTap,
+  }) {
+    return GestureDetector(
+      onTap: () {
+        onTap?.call();
+      },
+      child: Column(
+        children: [
+          Expanded(
+              child: Icon(
+            icon,
+            color: isSelect ? Colors.white : Colors.white70,
+          )),
+          Expanded(
+              child: Text(
+            title ?? "",
+            style: TextStyle(
+              color: isSelect ? Colors.white : Colors.white70,
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+
   Widget buildBody() {
     return Column(
-      children: [],
+      children: [
+        Expanded(child: Container()),
+        Expanded(child: Container()),
+      ],
     );
   }
 
