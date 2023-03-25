dcbc32142e47c4636c8e82b2cd7919257b740657
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-03-25 15:30:39 +0700

    add theme data

---
+++ lib/shared_product/widgets/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  var LIGHT_THEME = ThemeData();
+  var DARK_THEME = ThemeData();
+}
