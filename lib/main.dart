2cf86ba5467206c43d138674f1bef823fa2a1988
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-03-30 22:34:00 +0700

    remove wrong import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reel_t/models/video/video.dart';
-import 'package:reel_t/models/video/video_data.dart';
 import 'package:reel_t/screens/default/default_screen.dart';
 import '../generated/app_init.dart';
 import '../screens/welcome/welcome_screen.dart';
