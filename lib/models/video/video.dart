07a433b6fcc88ede08cd77cb75380590cc99ac13
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-03-31 21:21:48 +0700

    update video models and add push feature

--- lib/models/video/video.dart
+++ lib/models/video/video.dart
@@ -2,8 +2,8 @@
 import 'dart:convert';
 import '../../generated/abstract_model.dart';
 import '../../models/user_profile/user_profile.dart';
-import '../../models/comment/comment.dart';
 import '../../models/like/like.dart';
+import '../../models/comment/comment.dart';
 enum PublicMode { PUBLIC,PRIVATE }
 
 
