6df1440904220bd1ebb9aeca3979c1644231d355
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-04-06 13:34:58 +0700

    refactor verify otp

---
+++ lib/events/user/verify_email_otp/verify_email_otp_event.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:reel_t/models/email_otp/email_otp.dart';
+
+abstract class VerifyEmailOtpEvent {
+  final _db = FirebaseFirestore.instance.collection(EmailOtp.PATH);
+  Future<void> sendVerifyEmailOtpEvent(String email, String otp) async {
+    try {
+      var query = _db
+          .where(EmailOtp.verifyEmail_PATH, isEqualTo: email)
+          .where(EmailOtp.otpCode_PATH, isEqualTo: otp)
+          .where(EmailOtp.isVerify_PATH, isEqualTo: false);
+      var snapshot = await query.get();
+      if (snapshot.docs.isEmpty) {
+        onVerifyEmailOtpEventDone(
+            false, "OTP code is invalid, please try again");
+        return;
+      }
+      var emailOtp = EmailOtp.fromJson(snapshot.docs.last.data());
+      if (emailOtp.expireAt < DateTime.now().millisecondsSinceEpoch) {
+        onVerifyEmailOtpEventDone(
+            false, "OTP code is expired, please resend its");
+        return;
+      }
+
+      return onVerifyEmailOtpEventDone(true, "Success");
+    } catch (e) {
+      print(e);
+      onVerifyEmailOtpEventDone(
+          false, "Server is upgrade, please retry after 15 minutes");
+    }
+  }
+
+  void onVerifyEmailOtpEventDone(bool isVerified, String verifyStatus);
+}
