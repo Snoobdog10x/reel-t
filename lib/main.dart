fba57b5eccdd0446c67a5742d76b73740c40d480
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-04-09 00:43:48 +0700

    comment local run

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:reel_t/generated/app_init.dart';
 import 'package:reel_t/models/video/video.dart';
@@ -17,6 +18,8 @@ Future<void> main() async {
   // try {
   //   FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
   //   await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  //   FirebaseFunctions.instance.useFunctionsEmulator('localhost', 5001);
+  //   await FirebaseStorage.instance.useStorageEmulator("localhost", 9199);
   // } catch (e) {
   //   // ignore: avoid_print
   //   print(e);
