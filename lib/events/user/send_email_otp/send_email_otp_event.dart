613fcc38ad0239a972f984ed7ab41374b738ac8e
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-04-09 18:34:55 +0700

    call EmailOtp from cloud function

--- lib/events/user/send_email_otp/send_email_otp_event.dart
+++ lib/events/user/send_email_otp/send_email_otp_event.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:reel_t/models/email_otp/email_otp.dart';
 
 import '../../../generated/app_init.dart';
@@ -8,23 +9,12 @@ import '../../../generated/app_init.dart';
 abstract class SendEmailOtpEvent {
   final _appStore = AppInit.appStore;
   final _db = FirebaseFirestore.instance.collection(EmailOtp.PATH);
+  final _function = FirebaseFunctions.instance;
   Future<void> sendSendEmailOtpEvent(String email) async {
     try {
-      var OTP = _appStore.emailAuth.generateOTP();
-      var id = _db.doc().id;
-      var now = DateTime.now();
-      var emailLowerCase = email.toLowerCase();
-      var emailOTPJson = EmailOtp(
-        id: id,
-        otpCode: OTP,
-        verifyEmail: emailLowerCase,
-        isVerify: false,
-        createAt: now.millisecondsSinceEpoch,
-        expireAt: now.add(Duration(minutes: 5)).millisecondsSinceEpoch,
-      ).toJson();
-      await _db.doc(id).set(emailOTPJson);
-      var isSent = await _appStore.emailAuth.sendOTP(emailLowerCase);
-      onSendEmailOtpEventDone(isSent);
+      HttpsCallable callable = _function.httpsCallable("emailOtpGenerate");
+      var result = await callable.call(email);
+      onSendEmailOtpEventDone(true);
     } catch (e) {
       onSendEmailOtpEventDone(false);
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_functions/cloud_functions.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:reel_t/generated/app_init.dart';
-import 'package:reel_t/models/video/video.dart';
 import 'package:reel_t/screens/default/default_screen.dart';
-import '../screens/welcome/welcome_screen.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -15,16 +9,8 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  // try {
-  //   FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
-  //   await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-  //   FirebaseFunctions.instance.useFunctionsEmulator('localhost', 5001);
-  //   await FirebaseStorage.instance.useStorageEmulator("localhost", 9199);
-  // } catch (e) {
-  //   // ignore: avoid_print
-  //   print(e);
-  // }
-  AppInit.init();
+  // AppInit.init(true);
+  AppInit.init(false);
   runApp(
     MaterialApp(
       home: DefaultScreen(),
