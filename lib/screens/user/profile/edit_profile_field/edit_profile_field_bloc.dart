f9f41faeccdcb6fcec5f84a8ed47abe959c61818
huit <thng.huy1202@gmail.com>
2023-04-19 12:27:15 +0700

    create edit profie function

--- lib/screens/user/profile/edit_profile_field/edit_profile_field_bloc.dart
+++ lib/screens/user/profile/edit_profile_field/edit_profile_field_bloc.dart
@@ -1,12 +1,42 @@
+import '../../../../events/user/update_user_profile/update_user_profile_event.dart';
 import '../../../../generated/abstract_bloc.dart';
 import '../../../../models/user_profile/user_profile.dart';
 import 'edit_profile_field_screen.dart';
 
-class EditProfileFieldBloc extends AbstractBloc<EditProfileFieldScreenState> {
+class EditProfileFieldBloc extends AbstractBloc<EditProfileFieldScreenState>
+    with UpdateUserProfileEvent {
+  late UserProfile currentUserProfile;
   void init() {
     currentUserProfile = appStore.localUser.getCurrentUser();
     notifyDataChanged();
   }
 
-  late UserProfile currentUserProfile;
+  void updateUserFullName(String newUserFullName) {
+    currentUserProfile.fullName = newUserFullName;
+    notifyDataChanged();
+    sendUpdateUserProfileEvent(currentUserProfile);
+    state.startLoading();
+  }
+
+  void updateUserName(String newUserName) {
+    currentUserProfile.userName = newUserName;
+    notifyDataChanged();
+    sendUpdateUserProfileEvent(currentUserProfile);
+    state.startLoading();
+  }
+
+  void updateBio(String newBio) {
+    currentUserProfile.bio = newBio;
+    notifyDataChanged();
+    sendUpdateUserProfileEvent(currentUserProfile);
+    state.startLoading();
+  }
+
+  @override
+  void onUpdateUserProfileEventDone(UserProfile? newUserProfile) {
+    state.stopLoading();
+    state.popTopDisplay();
+    // TODO: implement onUpdateUserProfileEventDone
+    appStore.localUser.login(newUserProfile!);
+  }
 }
