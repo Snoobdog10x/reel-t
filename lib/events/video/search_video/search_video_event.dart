10f5ca4408445dae62ba8be449bda9c157b5765c
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-05-02 23:29:12 +0700

    update search by HashTags

--- lib/events/video/search_video/search_video_event.dart
+++ lib/events/video/search_video/search_video_event.dart
@@ -1,8 +1,7 @@
 import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:reel_t/events/user/search_user/search_user_event.dart';
+import 'package:reel_t/models/hash_tag/hash_tag.dart';
 import 'package:reel_t/models/user_profile/user_profile.dart';
 import 'package:reel_t/shared_product/services/firestore_search.dart';
 
@@ -15,10 +14,14 @@ abstract class SearchVideoEvent {
     List<UserProfile> userProfile = const [],
   }) async {
     try {
+      searchText = searchText.replaceAll("#", "");
+
       List<Future<List<Video>>> searchResults = [];
       searchResults.add(_searchVideoByUser(searchText, userProfile));
       searchResults.add(_searchVideoByTitle(searchText));
+      searchResults.add(_searchVideoByHashtag(searchText));
       var result = _mergeVideosList(await Future.wait(searchResults));
+      
       onSearchVideoEventDone(result);
     } catch (e) {
       print(e);
@@ -52,14 +55,35 @@ abstract class SearchVideoEvent {
     return searchResult.map((e) => Video.fromJson(e.data())).toList();
   }
 
+  Future<List<Video>> _searchVideoByHashtags(List<HashTag> hashTags) async {
+    List<Future<QuerySnapshot<Map<String, dynamic>>>> videoByHashtag = [];
+    final db = FirebaseFirestore.instance.collection(Video.PATH);
+    hashTags.forEach((hashTag) {
+      var hashTagName = hashTag.hashTagName;
+      videoByHashtag
+          .add(db.where(Video.hashtag_PATH, arrayContains: hashTagName).get());
+    });
+    Set<Video> videos = {};
+    (await Future.wait(videoByHashtag)).forEach((query) {
+      query.docs.forEach((doc) {
+        var video = Video.fromJson(doc.data());
+        videos.add(video);
+      });
+    });
+    return videos.toList();
+  }
+
   Future<List<Video>> _searchVideoByHashtag(String searchText) async {
     var searchResult = await _firestoreSearch.searchStream(
-      collectionPaths: Video.PATH,
-      searchByPath: Video.title_PATH,
+      collectionPaths: HashTag.PATH,
+      searchByPath: HashTag.id_PATH,
       searchText: searchText,
     );
     if (searchResult.isEmpty) return [];
-    return searchResult.map((e) => Video.fromJson(e.data())).toList();
+    List<HashTag> hashTags =
+        searchResult.map((e) => HashTag.fromJson(e.data())).toList();
+
+    return await _searchVideoByHashtags(hashTags);
   }
 
   List<Video> _mergeVideosList(List<List<Video>> videosList) {
